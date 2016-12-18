17652d412a31499bc5ad47cacacb900ff6fb9a17
zntemel <zntemel@gmail.com>
2016-12-18 21:39:26 +0200

    Fix pg migrations

--- db/migrate/20161216201037_create_avarage_flight_pricings.rb
+++ db/migrate/20161216201037_create_avarage_flight_pricings.rb
@@ -1,11 +1,17 @@
 class CreateAvarageFlightPricings < ActiveRecord::Migration
   def change
     create_table :avarage_flight_pricings do |t|
-      t.references :destination_city, index: true, foreign_key: true
-      t.references :origin_city, index: true, foreign_key: true
+      # t.references :destination_city, index: true, foreign_key: true
+      # t.references :origin_city, index: true, foreign_key: true
       t.bigint :price
 
       t.timestamps null: false
     end
+
+    add_reference :avarage_flight_pricings, :destination_city, references: :cities, index: true
+    add_foreign_key :avarage_flight_pricings, :cities, column: :city_id
+
+    add_reference :avarage_flight_pricings, :origin_city, references: :cities, index: true
+    add_foreign_key :avarage_flight_pricings, :cities, column: :city_id
   end
 end
